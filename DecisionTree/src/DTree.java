f8ef1fc588088151cac2c53609c0c19f17e0cc43
james <qusz88@gmail.com>
2016-02-04 21:42:46 -0600

    test

--- DecisionTree/src/DTree.java
+++ DecisionTree/src/DTree.java
@@ -1,8 +1,14 @@
+import java.util.*;
+import java.io.*;
+
 
 public class DTree {
 
 }
 
 class Node{
+	private String name;
+	private Node[] child;
+	private ArrayList<ArrayList<String>> data;
 	
 }
